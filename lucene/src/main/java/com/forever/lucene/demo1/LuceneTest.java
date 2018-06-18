ffc406ab01da112e4699c6e31457ef2d985f773d
forever <857173891@qq.com>
2018-06-18 23:55:25 +0800

    全文检索

--- lucene/src/main/java/com/forever/lucene/demo1/LuceneTest.java
+++ lucene/src/main/java/com/forever/lucene/demo1/LuceneTest.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,23 +25,39 @@ public class LuceneTest {
     @Test
     public void testCreateIndex() throws IOException {
         //获取原始文档
-        File fileDir = new File("D:\\luceneFile");
-        File[] files = fileDir.listFiles();
+        File[] files = getFiles();
 
-        //将原始文档 封装成 document
+        //将原始文档 转换成 document
+        List<Document> docList = getDocs(files);
+
+        //创建索引
+        //使用IndexWriter 将doc写入到索引库
+        Directory dir = FSDirectory.open(Paths.get("index"));
+        IndexWriterConfig config = new IndexWriterConfig();
+        IndexWriter indexWriter = new IndexWriter(dir,config);
+
+        indexWriter.addDocuments(docList);
+        indexWriter.commit();
+        indexWriter.close();
+    }
+
+
+    private List<Document> getDocs(File[] files ) throws IOException {
+        //将原始文档 转换成 document
         List<Document> docList = new ArrayList<>();
         for (File file : files) {
             Document doc = new Document();
             /**
              * 创建Field域
              */
-            //fieldName域
             Field nameField = new TextField("nameField",file.getName(),Field.Store.YES);
             String content = FileUtils.readFileToString(file, "utf-8");
             Field contentField = new TextField("contentField",content,Field.Store.YES);
             Field pathField = new StoredField("pathField",file.getPath());
             Field sizeField = new LongPoint("sizeField",FileUtils.sizeOf(file));
-
+            /**
+             * Document 和 Field 绑定
+             */
             doc.add(nameField);
             doc.add(contentField);
             doc.add(pathField);
@@ -47,20 +65,21 @@ public class LuceneTest {
 
             docList.add(doc);
         }
-
-        /**
-         * 创建indexWriter对象 将数据写入到索引
-         */
-        Directory dir = FSDirectory.open(Paths.get("D:\\index"));
-        IndexWriterConfig config = new IndexWriterConfig();
-        IndexWriter indexWriter = new IndexWriter(dir,config);
-        indexWriter.addDocuments(docList);
-        indexWriter.commit();
-        indexWriter.close();
+        return docList;
     }
 
 
-
+    /**
+     * 获取原始文档
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    private File[] getFiles() throws UnsupportedEncodingException {
+        String fileDirPath = URLDecoder.decode( this.getClass().getResource("/").getPath()+"luceneFile", "utf-8");
+        File fileDir = new File(fileDirPath);
+        File[] files = fileDir.listFiles();
+        return  files;
+    }
 
 
 
